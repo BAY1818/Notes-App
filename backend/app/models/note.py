5085284d9699105d433634dabc0a36276a9e1107
Baygeldi <baygeldi0718@gmail.com>
2025-11-16 00:22:12 +0800

    CommentAddded

--- backend/app/models/note.py
+++ backend/app/models/note.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Column,Integer,String,Text,DataTime
 from sqlalchemy.sql import func 
-from ..db.session import Base
+from ..db.session import Base   
 
 #Note derived class for create table for database 
 class Note(Base):
     __tablename__ = "Note"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False)
-    content = Column(Text,nullable=True)
-    created_at = Column(DataTime(timezone = True), server_default=func.now())
+    id = Column(Integer, primary_key=True, index=True)          # index is counting for whenever tables created
+    title = Column(String(100), nullable=False)                 # nullable=False can't create table if title is empty
+    content = Column(Text,nullable=True)                
+    created_at = Column(DataTime(timezone = True), server_default=func.now())   # saves time automatically whenever table is created 
