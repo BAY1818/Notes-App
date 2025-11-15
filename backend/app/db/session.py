46cdcb41138851b84d26950365f55ed239fd6cb9
Baygeldi <baygeldi0718@gmail.com>
2025-11-15 23:40:37 +0800

    Engine_Connected

---
+++ backend/app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .database import engine
+
+
+# Create a session factory 
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine )
+
+
+# Base Class for all models
+Base = declarative_base()
+
+# Dependency for FastAPI routes 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
