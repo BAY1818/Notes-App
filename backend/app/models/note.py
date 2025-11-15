17b330ecc7a09b2acb103b4fd550cae5b09efac8
Baygeldi <baygeldi0718@gmail.com>
2025-11-16 00:13:47 +0800

    TableCreated

---
+++ backend/app/models/note.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column,Integer,String,Text,DataTime
+from sqlalchemy.sql import func 
+from ..db.session import Base
+
+
+class Note(Base):
+    __tablename__ = "Note"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    content = Column(Text,nullable=True)
+    created_at = Column(DataTime(timezone = True), server_default=func.now())
