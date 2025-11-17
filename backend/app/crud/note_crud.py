a6dcf4ddf5b2c5682086d1a8c2bbbf284f769dde
Baygeldi <baygeldi0718@gmail.com>
2025-11-17 23:21:29 +0800

    UpdateFuncAdded

--- backend/app/crud/note_crud.py
+++ backend/app/crud/note_crud.py
@@ -14,8 +14,23 @@ def note_create(db:Session, note_data:CreateNote):
 def get_notes(db:Session):
     return db.query(Note).all()
 
-def get_note_id(db:Session):
-    return db.query(Note.id).all()
+def get_note_id(db:Session, note_id : int):
+    return db.query(Note).filter(Note.id == note_id).first()
+
+def note_update(db:Session, note_id : int, note_data:UpdateNote):
+    note = get_note_id(db,note_id)
+    if not note:
+        return None
+    
+    if note_data.title is not None:
+        note.title = note_data.title
+    if note_data.content is not None:
+        note.content = note_data.content
+
+    db.refresh()
+    db.commit() 
+
+
 
 
     
